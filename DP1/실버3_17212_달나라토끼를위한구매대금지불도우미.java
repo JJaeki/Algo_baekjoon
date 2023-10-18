2645394fefb96ecc6a498182442d06c5b65f18a4
JJaeki <wodud9515@naver.com>
2023-10-18 18:56:54 +0900

    Upload: 10.18 / [달나라 토끼를 위한 구매대금 지불 도우미] / 144ms

---
+++ DP1/실버3_17212_달나라토끼를위한구매대금지불도우미.java
@@ -0,0 +1,26 @@
+package DP1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 실버3_17212_달나라토끼를위한구매대금지불도우미 {
+
+    private static final int[] coin = {1, 2, 5, 7};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, 100001);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (i - coin[j] < 0) continue;
+                dp[i] = Math.min(dp[i], dp[i - coin[j]] + 1);
+            }
+        }
+        System.out.print(dp[n]);
+    }
+}
