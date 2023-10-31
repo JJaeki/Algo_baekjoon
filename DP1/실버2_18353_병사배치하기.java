a9d71fa73915a3658cfc67f77a27f0b4db868ef3
jaeyoung <wodud9515@naver.com>
2023-10-31 21:14:02 +0900

    Upload: 10.31 / [병사 배치하기] / 164ms

---
+++ DP1/실버2_18353_병사배치하기.java
@@ -0,0 +1,44 @@
+package DP1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 실버2_18353_병사배치하기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] soldiers = new int[N];
+        int[] dp = new int[N + 1];
+
+        Arrays.fill(dp, 1);
+        String temp = br.readLine();
+        StringTokenizer st = new StringTokenizer(temp);
+        int k = 0;
+        while (st.hasMoreTokens()) {
+            soldiers[k++] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (soldiers[i] < soldiers[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int max = 0;
+        Arrays.sort(dp);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(Integer.toString(N - dp[N]));
+        bw.flush();
+        bw.close();
+    }
+
+}
