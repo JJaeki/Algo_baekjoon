5aeaa14d459bd8a0d2ba4aebb2b43e73fdf9b050
jaeyoung <wodud9515@naver.com>
2023-11-03 19:56:44 +0900

    Upload: 11.03 / [간식 파티] / 152ms

---
+++ DP1/실버2_20162_간식파티.java
@@ -0,0 +1,37 @@
+package DP1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.function.Function;
+
+public class 실버2_20162_간식파티 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Function<String, Integer> stoi = Integer::parseInt;
+        int n = stoi.apply(br.readLine());
+        int[] input = new int[n];
+        for (int i = 0; i < n; i++) {
+            input[i] = stoi.apply(br.readLine());
+        }
+        int result = cal(input, n);
+        System.out.println(result);
+    }
+
+    private static int cal(int[] input, int n) {
+        int[] dp = Arrays.copyOf(input, n);
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result = Math.max(result, input[i]);
+            for (int j = i + 1; j < n; j++) {
+                if (input[i] < input[j] && dp[i] + input[j] > dp[j]) {
+                    dp[j] = dp[i] + input[j];
+                    result = Math.max(result, dp[j]);
+                }
+            }
+        }
+        return result;
+    }
+
+}
