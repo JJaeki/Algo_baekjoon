fc1a8772d313a03d6e764f3506d71a331c3c378a
JJaeki <wodud9515@naver.com>
2024-01-28 15:05:31 +0900

    Upload: 1.28 / [카드 정렬하기] / 360ms

---
+++ greedy/골드4_1715_카드정렬하기.java
@@ -0,0 +1,23 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class 골드4_1715_카드정렬하기 {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        while (n-- > 0) pq.add(Integer.parseInt(br.readLine()));
+        int sum = 0;
+        while (pq.size() > 1) {
+            int tmp = pq.poll() + pq.poll();
+            sum += tmp;
+            pq.add(tmp);
+        }
+        System.out.println(sum);
+    }
+}
