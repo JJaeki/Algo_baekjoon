2d0798e091a278ee8b69cb7b9f6f261891d77e8b
JJaeki <wodud9515@naver.com>
2024-02-21 17:17:41 +0900

    Upload: 2.21 / [단어 맞추기] / 120ms

---
+++ implementation/실버1_9081_단어맞추기.java
@@ -0,0 +1,50 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+public class 실버1_9081_단어맞추기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bfr.readLine());
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(solution(bfr.readLine()));
+        }
+    }
+
+    static String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+
+        char[] ary = s.toCharArray();
+        int idx1 = -1, idx2 = 0;
+
+        for (int i = n - 1; i > 0; i--) {
+            if (ary[i - 1] < ary[i]) {
+                idx1 = i - 1;
+                break;
+            }
+        }
+
+        if (idx1 == -1) return s;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (ary[idx1] < ary[i]) {
+                idx2 = i;
+                break;
+            }
+        }
+
+        char tmp = ary[idx1];
+        ary[idx1] = ary[idx2];
+        ary[idx2] = tmp;
+
+        Arrays.sort(ary, idx1 + 1, n);
+        for (char c : ary) sb.append(c);
+
+        return sb.toString();
+    }
+}
