b0e6100739aecd1435123c7e1bdeb90c7663afa8
JJaeki <wodud9515@naver.com>
2024-03-13 14:50:32 +0900

    Upload: 3.13 / [N과 M (12)] / 176ms

---
+++ backtracking/실버2_15666_N과M12.java
@@ -0,0 +1,53 @@
+package backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 실버2_15666_N과M12 {
+
+    static int N, M;
+    static int[] arr, graph;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        graph = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        DFS(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void DFS(int depth, int value) {
+        if (depth == M) {
+            for (int x : graph) {
+                sb.append(x + " ");
+            }
+            sb.append("\n");
+        } else {
+            int before = 0;
+            for (int i = value; i < N; i++) {
+                if (before != arr[i]) {
+                    graph[depth] = arr[i];
+                    before = arr[i];
+                    DFS(depth + 1, i);
+                } else {
+                    continue;
+                }
+            }
+        }
+    }
+}
