0494467722dea8c2c75276da2457f17518d60131
JJaeki <wodud9515@naver.com>
2024-04-29 16:31:33 +0900

    Upload: 4.29 / [5] / 124ms

---
+++ backtracking/골드3_3165_5.java
@@ -0,0 +1,78 @@
+package backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/3165
+
+public class 골드3_3165_5 {
+
+    private static final char FIVE = '5';
+    private static StringBuilder n;
+    private static int k;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = new StringBuilder(String.valueOf(Long.parseLong(st.nextToken()) + 1)).reverse();
+        k = Integer.parseInt(st.nextToken());
+        br.close();
+
+        backtrack(0, false);
+        addRemaining();
+        System.out.println(new StringBuilder(n).reverse());
+    }
+
+    private static void backtrack(int idx, boolean flag) {
+        if (idx >= n.length() || count() >= k) {
+            return;
+        }
+
+        if (flag) {
+            n.setCharAt(idx - 1, FIVE);
+        }
+
+        if (count() >= k) {
+            return;
+        }
+
+        if (n.charAt(idx) < FIVE) {
+            n.setCharAt(idx, FIVE);
+            backtrack(idx + 1, false);
+        } else if (n.charAt(idx) > FIVE) {
+            n.setCharAt(idx, '0');
+            roundUp(idx);
+            backtrack(idx + 1, true);
+        } else {
+            backtrack(idx + 1, false);
+        }
+    }
+
+    private static int count() {
+        int ct = 0;
+        for (int i = 0; i < n.length(); i++) {
+            if (n.charAt(i) == FIVE) {
+                ct++;
+            }
+        }
+        return ct;
+    }
+
+    private static void roundUp(int idx) {
+        long add = 10;
+        for (int i = 0; i < idx; i++) {
+            add *= 10;
+        }
+        n = new StringBuilder(String.valueOf(Long.parseLong(n.reverse().toString()) + add)).reverse();
+    }
+
+    private static void addRemaining() {
+        int ct = count();
+        for (int i = ct; i < k; i++) {
+            n.append(FIVE);
+        }
+    }
+}
